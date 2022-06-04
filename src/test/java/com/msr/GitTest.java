b717c1a0cbde34979bec892f78f11daff0caa611
Hwj459807707 <106853741+Hwj459807707@users.noreply.github.com>
2022-06-04 13:40:59 +0800

    pull commit

--- src/test/java/com/msr/GitTest.java
+++ src/test/java/com/msr/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("master1");
         System.out.println("hot-text");
         System.out.println("push test");
+         System.out.println("pull test");
+        
     }
 }
